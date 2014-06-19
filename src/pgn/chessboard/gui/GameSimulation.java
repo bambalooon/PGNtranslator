d077b59081905f47e77e09eafe83f4dc8bbd915a
bambalooon <k.bb@wp.pl>
2014-06-19 14:51:40 +0200

    simulation

--- src/pgn/chessboard/gui/GameSimulation.java
+++ src/pgn/chessboard/gui/GameSimulation.java
@@ -5,6 +5,10 @@ import pgn.chessboard.figures.Figure;
 import pgn.tokenizer.TokenizedGame;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,21 +21,46 @@ public class GameSimulation {
     private TokenizedGame game;
     private ChessBoardPanel panel;
     private ChessBoard chessBoard;
+    private List<Figure[][]> boards = new LinkedList<>();
+    private ListIterator<Figure[][]> iterator = boards.listIterator();
 
     public GameSimulation(ChessBoardPanel panel, TokenizedGame game) {
         this.panel = panel;
         this.game = game;
         this.chessBoard = new ChessBoard();
-        this.drawBoard();
+        this.drawStartBoard();
     }
 
 
-    public void drawBoard() {
+    public void drawStartBoard() {
         Object[][] boardTemp = chessBoard.getBoardCopy();
         if(boardTemp instanceof Figure[][]) {
             Figure[][] board = (Figure[][]) boardTemp;
+            boards.add(board);
+//            if(iterator.hasNext())
+//                iterator.next();
             panel.updateBoard(board);
             panel.repaint();
         }
     }
+
+    public void drawNextBoard() {
+        if(iterator.hasNext()) {
+            Figure[][] board = iterator.next();
+            panel.updateBoard(board);
+            panel.repaint();
+        }
+        else {
+
+        }
+    }
+
+    public void drawPrevBoard() {
+        if(iterator.hasPrevious()) {
+            Figure[][] board = iterator.previous();
+            panel.updateBoard(board);
+            panel.repaint();
+        }
+    }
+
 }
